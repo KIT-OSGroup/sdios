670a786b126b621a72dc7556cec3409e3d2198eb
Jan Stoess <stoess@ira.uka.de>
2009-05-14 10:57:15 +0200

    Initial revision

---
+++ lib/sdi/logging.cc
@@ -0,0 +1,15 @@
+//
+// File: lib/sdi/logging.cc
+//
+// Description: Logging messages
+//
+
+#include <stdlib.h>
+
+#include <sdi/types.h>
+#include <sdi/sdi.h>
+
+
+void LogMessage (char* message) {
+    panic ("LogMessage () is not implemented. Put your code in here");
+}
